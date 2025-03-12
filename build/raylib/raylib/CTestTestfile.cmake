32329e15f391392ade7f016a1764ca5463396ffb
Hoang Gia <giagia8765@gmail.com>
2025-03-12 07:32:04 +0700

    tao file Arrrow

---
+++ build/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Downloads/sproject/DataStructure/raylib/src
+# Build directory: D:/Downloads/sproject/DataStructure/build/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
